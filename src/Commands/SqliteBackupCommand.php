139e3c9149e3f6df52948f9dc6873ee4ac3f9e48
arnebr <arnebr@gmail.com>
2024-06-16 09:29:29 +0000

    chore: Add SqliteBackupCommand for SQLite database backup

---
+++ src/Commands/SqliteBackupCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AHOI\SqliteOptimize\Commands;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class SqliteBackupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sqlite:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup SQLite database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $database = config('database.connections.sqlite.database');
+        $filename = 'backup-'.now()->timestamp.'.sql';
+        $this->info('Starting SQLite backup...');
+
+        try {
+            // Create a backup of the SQLite database
+            File::copy( $database, database_path('backups/'.$filename));
+
+            $this->info('SQLite backup created successfully at: ' . database_path('backups/'.$filename));
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $this->error('Failed to create SQLite backup: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+    }
+}
