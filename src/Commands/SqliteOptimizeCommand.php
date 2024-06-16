8cef5728787503daeb071db31e520c43c41a0f0c
arnebr <arnebr@users.noreply.github.com>
2024-06-16 08:32:00 +0000

    Fix styling

--- src/Commands/SqliteOptimizeCommand.php
+++ src/Commands/SqliteOptimizeCommand.php
@@ -2,12 +2,12 @@
 
 namespace AHOI\SqliteOptimize\Commands;
 
-use LogicException;
 use Illuminate\Console\Command;
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\SQLiteConnection;
-use Illuminate\Database\ConnectionInterface;
+use LogicException;
 
 class SqliteOptimizeCommand extends Command
 {
@@ -19,17 +19,16 @@ class SqliteOptimizeCommand extends Command
     public $signature = 'sqlite:wal-enable
                             {connection=sqlite : The connection to enable WAL journal}';
 
-     /**
+    /**
      * The console command description.
      *
      * @var string
      */
     public $description = 'Enables WAL journal on SQLite databases as performance optimization.';
 
-     /**
+    /**
      * Execute the console command.
      *
-     * @param  \Illuminate\Database\DatabaseManager $manager
      * @return int
      */
     public function handle(DatabaseManager $manager)
@@ -45,14 +44,13 @@ class SqliteOptimizeCommand extends Command
         }
 
         $this->info("The '$connection' connection has been set as [$journal] journal mode.");
+
         return Command::SUCCESS;
     }
 
     /**
      * Returns the Database Connection
      *
-     * @param  \Illuminate\Database\DatabaseManager $manager
-     * @param  string $connection
      * @return \Illuminate\Database\Connection
      */
     protected function getDatabase(DatabaseManager $manager, string $connection)
@@ -60,7 +58,7 @@ class SqliteOptimizeCommand extends Command
         $db = $manager->connection($connection);
 
         // We will throw an exception if the database is not SQLite
-        if(!$db instanceof SQLiteConnection) {
+        if (! $db instanceof SQLiteConnection) {
             throw new LogicException("The '$connection' connection must be sqlite, [{$db->getDriverName()}] given.");
         }
 
@@ -70,7 +68,6 @@ class SqliteOptimizeCommand extends Command
     /**
      * Sets the Journal Mode to WAL
      *
-     * @param  \Illuminate\Database\ConnectionInterface $connection
      * @return bool
      */
     protected function setWalJournalMode(ConnectionInterface $connection)
@@ -81,7 +78,6 @@ class SqliteOptimizeCommand extends Command
     /**
      * Returns the current Journal Mode of the Database Connection
      *
-     * @param  \Illuminate\Database\ConnectionInterface $connection
      * @return string
      */
     protected function getJournalMode(ConnectionInterface $connection)
